193e9b7a1713ac016caa3e8f9678beb12fd122c9
Travis Chen <travis.chen@everchanging.dev>
2024-06-22 17:02:09 -0700

    Update to use importlib

--- derived_metadata/__init__.py
+++ derived_metadata/__init__.py
@@ -3,6 +3,7 @@
 Taken from https://stackoverflow.com/a/3365846/4330447.
 """
 
+import importlib
 import pkgutil
 
 __all__ = []
@@ -10,5 +11,5 @@ for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
   if module_name.endswith('_test'):
     continue
   __all__.append(module_name)
-  _module = loader.find_module(module_name).load_module(module_name)
+  _module = importlib.import_module(f'.{module_name}', 'derived_metadata')
   globals()[module_name] = _module
